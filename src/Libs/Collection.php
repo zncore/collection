68125e3d681a6bbce75a927f557b90655d879336
vitaliy <zndev@ya.ru>
2022-08-28 16:57:37 +0600

    upd

--- src/Libs/Collection.php
+++ src/Libs/Collection.php
@@ -15,4 +15,22 @@ class Collection extends \Doctrine\Common\Collections\ArrayCollection implements
     {
         return new static(array_reverse($this->toArray(), true));
     }
+
+    public function sortBy($key): self
+    {
+        $sorCallback = function ($item1, $item2) use ($key) {
+            $a = $item1->{$key};
+            $b = $item2->{$key};
+
+            if ($a === $b) {
+                return 0;
+            }
+            return $a < $b ? -1 : 1;
+        };
+
+        $iterator = $this->getIterator();
+        $iterator->uasort($sorCallback);
+        return new static(iterator_to_array($iterator));
+//        dd($sorted);
+    }
 }
